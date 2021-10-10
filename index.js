7e13ed685a7bf2b80e38e8c5b15d6e38a0f6694f
tusharravenclaw <72013857+tusharravenclaw@users.noreply.github.com>
2021-10-10 23:52:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["Content Creator", "Content Writer", "Podcaster", "Front-End Developer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
